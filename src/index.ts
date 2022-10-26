395971756af5ea97000aa461e34b70d8cff74fd4
Zai Santillan <57343545+plskz@users.noreply.github.com>
2022-10-27 05:58:14 +0800

    chore: use ts (#22)
    * chore: use ts
    
    * chore: apply suggested changes

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,24 @@
-import { exec } from 'child_process';
 import fs from 'fs';
-
-const RED = '\x1b[31m%s\x1b[0m';
-const GREEN = '\x1b[32m%s\x1b[0m';
-
-const weeks = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-];
-const months = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
-
-// -------------------------------------------------------------
-
-const pad = (x, y = 3) => x.toString().padStart(y, '0');
-const error = (msg) => console.log(RED, msg);
-const success = (msg) => console.log(GREEN, msg);
-
-const open = (path, vscode) => {
-  if (!vscode) return;
-
-  exec(`code ${path}`, (error, stdout, stderr) => {
-    if (error) return console.log(`error: ${error.message}`);
-    if (stderr) return console.log(`stderr: ${stderr}`);
-    // console.log(`stdout: ${stdout}`);
-    success(`\n\topening ${path}`);
-  });
-};
-
-// -------------------------------------------------------------
+import { error,MONTHS,open, pad, success, WEEKS } from './utils';
 
 /**
- * generates a day.md template
- * @param {Object} user - challenger infos
  * @param {string} user.name - any name that you're comfortable with
  * @param {string} user.github - your github username
  * @param {string} user.start - the date that you will start this challenge (e.g. m/d/yyyy)
  * @param {number} [user.round=1] - the round of the current challenge
  * @param {boolean} [user.vscode=true] - whether to open the file after generating it
  */
-export function run({ name, github, start, round = 1, vscode = true }) {
+
+interface User {
+  name: string;
+  github: string;
+  start: string;
+  round?: number;
+  vscode?: boolean;
+}
+
+/** Generates a `day.md` template */
+export function run({ name, github, start, round = 1, vscode = true }: User) {
   const arg2 = process.argv[2];
   const validDate = /^(?:(?:(?:0?[13578]|1[02])(\/|-|\.)31)\1|(?:(?:0?[1,3-9]|1[0-2])(\/|-|\.)(?:29|30)\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:0?2(\/|-|\.)29\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:(?:0?[1-9])|(?:1[0-2]))(\/|-|\.)(?:0?[1-9]|1\d|2[0-8])\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
   let d = new Date();
@@ -84,8 +46,8 @@ export function run({ name, github, start, round = 1, vscode = true }) {
 
   const CURRENT = `${month + 1}/${day}/${year}`;
 
-  const weekName = weeks[d.getDay()];
-  const monthName = months[month];
+  const weekName = WEEKS[d.getDay()];
+  const monthName = MONTHS[month];
 
   const diff = Math.floor((Date.parse(CURRENT) - Date.parse(start)) / 86400000) + 1;
   const [prevDay, nextDay] = [diff - 1, diff + 1];
